7442ec9ec880b4cf5fe0dd77b6727b1f30cc4f0c
KIRTI1068 <kirti171407.cse@chitkara.edu.in>
2019-12-04 14:22:49 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
+import './App.css';
 
 class App extends Component {
+
   render() {
+
+    let subscribers = [
+      {
+        id: 1,
+        name: "Kirti Goel",
+        phone: "8708354923"
+      },
+      {
+        id: 2,
+        name: "Khushboo Jindal",
+        phone: "9999999999"
+      },
+      {
+        id: 3,
+        name: "Khushboo Sharma",
+        phone: "9993474847"
+      }
+    ];
+
     return (
       <div>
-       
-        <Header/>
-        <button>Add</button>
-        <div>
-          <span>Name</span><br />
-          <span>Phone</span>
+        <Header />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+
+          {
+            subscribers.map(sub => {
+              return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+              </div>
+            })
+          }
         </div>
       </div>
     );

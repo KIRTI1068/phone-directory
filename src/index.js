7f1559f55d2b77320a4bdb213095a0b83c05937e
KIRTI1068 <kirti171407.cse@chitkara.edu.in>
2019-12-09 13:30:13 +0530

    Added state in ‘AddSubscriber’ and ‘App’ components

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import  * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
-ReactDOM.render(<AddSusbscriber />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();

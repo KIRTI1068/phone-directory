e6f237dc35690f8c787ef927d839e7f58943cf20
KIRTI1068 <kirti171407.cse@chitkara.edu.in>
2019-12-10 15:00:41 +0530

    Updates

---
+++ precache-manifest.5da90036883a9b568cae6002ed8d28ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9625c6b722ef2b63aeb7ca2266d5526f",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "9586876a312aac13cf56",
+    "url": "/phone-directory/static/css/main.b089eaf0.chunk.css"
+  },
+  {
+    "revision": "264ed69fc98d80809fce",
+    "url": "/phone-directory/static/js/2.7fcfd888.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/phone-directory/static/js/2.7fcfd888.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9586876a312aac13cf56",
+    "url": "/phone-directory/static/js/main.fa6e8d1c.chunk.js"
+  },
+  {
+    "revision": "505cdc9333590f35c409",
+    "url": "/phone-directory/static/js/runtime-main.53d98e78.js"
+  }
+]);
